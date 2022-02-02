89915d0dfa113f0abca7328a29318cd6ebd4962a
amalsabu59 <amalsabu59@gmail.com>
2022-02-02 18:23:12 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 //importing
 import express from 'express';
+import  mongoose  from 'mongoose';
 
 // app config
 
 const app = express()
-const port = process.env.PORT || 9000
+const port = process.env.PORT || 9000;
 
-app.get('/',(req,res)=>{
+const connection_url = "mongodb+srv://admin:tDlSZ8ksrSJKNX8C@cluster0.efthy.mongodb.net/chathouse?retryWrites=true&w=majority";
+
+mongoose.connect(connection_url, {
+    useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true,
+    serverSelectionTimeoutMS: 5000
+  }).catch(err => console.log(err.reason));
+
+app.get('/',(req,res)=>
     res.status(200).send('hellow world')
-})
+);
 
-app.listen(port,()=>console.log(`listening in localhost:${port}`))
+app.listen(port,()=>console.log(`listening on localhost:${port}`))
